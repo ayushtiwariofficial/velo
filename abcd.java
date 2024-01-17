2d125a6f3f41ce1adaf886b9765a49c1941b0f29
DIPANSHU-KAPOOR <dipanshu.kapoor2002@gmail.com>
2024-01-17 21:40:08 +0530

    Update abcd.java

--- abcd.java
+++ abcd.java
@@ -1,2 +1,21 @@
 hello brother kya krrhe ho so jao
 bhai jldi jldi array left shift ka code bhj
+
+  class Test {
+
+  @Override
+  public String toString() {
+    return "object";
+  }
+}
+
+class Main {
+  public static void main(String[] args) {
+
+    // create an object of the Test class
+    Test obj = new Test();
+
+    // print the object
+    System.out.println(obj);
+  }
+}
